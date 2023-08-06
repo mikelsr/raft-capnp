fd821c67b37eb35836e9231f63e892fb1329232f
mikelsr <mikelsr@pm.me>
2023-08-06 20:43:06 +0200

    Update example to start/stop a node

--- example/main.go
+++ example/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "github.com/mikelsr/raft-capnp/raft"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/mikelsr/raft-capnp/raft"
+)
 
 func main() {
-	_ = raft.New().
+	n := raft.New().
 		WithRaftConfig(raft.DefaultConfig()).
 		WithRaftStore(raft.DefaultRaftStore).
 		WithStorage(raft.DefaultStorage()).
 		WithRaftNodeRetrieval(raft.NilRaftNodeRetrieval).
 		WithOnNewValue(raft.NilOnNewValue)
+	defer func() {
+		time.Sleep(10 * time.Millisecond)
+		n.Stop(errors.New("bye"))
+	}()
+
+	n.Start(context.Background())
 }
